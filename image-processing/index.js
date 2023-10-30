4e77c49fc41a476c5755901d579c73b0140af064
Ironboy <ironboy@users.noreply.github.com>
2023-10-30 12:37:55 +0100

    changes to scaler

--- image-processing/index.js
+++ image-processing/index.js
@@ -1,6 +1,17 @@
-import { mkdirSync, rmSync, existsSync } from 'fs';
-import { dirname, join as pathJoin } from 'path';
+import {
+  mkdirSync, rmSync, existsSync,
+  readdirSync, statSync
+} from 'fs';
+import sharp from 'sharp';
+import { dirname, join as pathJoin, basename } from 'path';
 import { fileURLToPath } from 'url';
+import { writeFile } from 'fs/promises';
+
+// Settings
+const settings = {
+  jpgQuality: 70,
+  widths: [250, 500, 1000, 1500, 2000]
+}
 
 // The path to the directory this script is in
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -11,5 +22,43 @@ const destFolder = pathJoin(__dirname, '..', 'public', 'images', 'scaled');
 
 // Delete, if exists, and then (re)create the destination folder
 existsSync(destFolder) && rmSync(destFolder, { recursive: true, force: true });
+mkdirSync(destFolder);
 
-// Get a file listing of all files in the 
+// Get the jpg files to process
+let files = readDirRecursive(srcFolder)
+  .filter(x => x.slice(-4) === '.jpg');
+
+
+// Process files
+for (let file of files) {
+  let destPath = file.replace(srcFolder, destFolder);
+  let destFolderPath = dirname(destPath);
+  // create folder if not exists
+  !existsSync(destFolderPath) && mkdirSync(destFolderPath);
+  // scale image
+  for (let width of settings.widths) {
+    console.log('Scaling ' + basename(file), 'to width', width);
+    await sharp(file)
+      .resize({ width, quality: settings.jpgQuality })
+      .toFile(destPath.slice(0, -4) + '-' + width + 'w.jpg');
+  }
+}
+
+
+// read all files in a folder and its sub folders
+// (from StackOverflow)
+function readDirRecursive(dir) {
+  var results = [];
+  var list = readdirSync(dir);
+  list.forEach(function (file) {
+    file = dir + '/' + file;
+    var stat = statSync(file);
+    if (stat && stat.isDirectory()) {
+      results = results.concat(readDirRecursive(file));
+    } else {
+      results.push(file);
+    }
+  });
+  return results;
+}
+
